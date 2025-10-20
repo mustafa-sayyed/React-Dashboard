0cb9a1f7992182cc9b31d86f232cf74dd46edf26
Mustafa Sayyed <mustafasayyed2429@gmail.com>
2025-10-21 02:16:06 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <div className="h-screen w-screen bg-gray-900">
+      <h1 className="text-6xl text-red-600">Welcome To The Medify Dashboard</h1>
+    </div>
+  )
+}
+
+export default App
