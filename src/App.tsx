fa628f56ad6447f7967f0f7ef5f626828a8f606b
Mustafa Sayyed <mustafasayyed2429@gmail.com>
2025-10-21 21:56:19 +0530

    config Shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-
+import { Button } from "@/components/ui/button";
 
 function App() {
-
   return (
-    <div className="h-screen w-screen bg-gray-900">
-      <h1 className="text-6xl text-red-600">Welcome To The Medify Dashboard</h1>
+    <div className="h-screen w-screen dark:bg-gray-950 dark:text-white flex flex-col justify-center items-center ">
+      <h1 className="text-6xl text-center">Welcome To The Medify Dashboard</h1>
+      <div>
+        <Button className="bg-white text-black cursor-pointer mt-10">
+          Submit
+        </Button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
